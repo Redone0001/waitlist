685714f2e6ea6157841cd4d01b4abfa7c1d2a48a
Redone0001 <loick.coets@gmail.com>
2023-09-15 19:18:22 +0200

    [add] Home page for beans + logo

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -10,51 +10,25 @@ export function Home() {
         <NavButton to={`/legal`}>Legal</NavButton>
       </InputGroup>
       <Content>
-        <h2>Welcome to The Ditanian Fleet</h2>
+        <h2>Welcome to The Beancursion</h2>
 
         <p>
-          Hello capsuleers, we would like to introduce you to the EVE Online incursion community
-          called The Ditanian Fleet (TDF). We are a new-bro friendly group where all are welcome. We
+          Hello fellow bean, we would like to introduce you to the SIG Beancursion. 
+		  We are a new-bro friendly group where all are welcome. We
           have affordable starter fits, and we can help make you space rich, all we ask in return is
-          you follow our upgrade policy to help the community excel and follow the rules! We exclude
-          pilots that have a previous history of ganking, and we have a no tolerance policy for
-          racism, sexism or anti-social behaviour.
+          you follow our upgrade policy to help the SIG excel and follow the rules!
         </p>
         <p>
-          The Ditanian Fleet is an armor doctrine incursion community. We run Vanguards (10 person),
-          Assaults (20 person), and Headquarters (40 person) Incursion sites. We have fleet
-          commanders from all over the world, so you should be able to join a fleet nearly 24/7.
+          Beancursion is an armor doctrine incursion SIG. We run Vanguards (15 person),
+          Assaults (30 person), and Headquarters (60 person) Incursion sites.
         </p>
         <p>
           Check out our <NavLink to="/guide">Guides</NavLink> section for all the information you
           need to get started, see <NavLink to="/guide/newbro">new-bro guide</NavLink>,{" "}
           <NavLink to="/guide/xup">first fleet guide</NavLink> etc, also join the in-game chat
-          channel <em>TDF-Official</em>, where you will find the mailing lists for fits and other
+          channel <em>Sansha Containment Service</em>, where you will find the mailing lists for fits and other
           usual information in the MOTD (message of the day), we look forward to flying with you!
-          Disclaimer, TDF will never recruit or give loans to new players, any such activity is not
-          endorsed by TDF and should be met with skepticism!
         </p>
-        <h3>Why Fly TDF?</h3>
-        <ul>
-          <li>
-            Armor fleets are far safer, as your tank is passive and thus not cap dependent. If you
-            get neuted your tank will not turn off, this gives you that extra bit of room for error
-            should you need it.
-          </li>
-          <li>
-            With our low slots being used for tanking, the mid slots on our ships are free for
-            damage application mods such as webifiers and tracking computers, giving us great damage
-            application.
-          </li>
-          <li>
-            Our upgrade policy ensures that everyone in the fleet is constantly improving and
-            contributing to that ever-important factor, your ISK per hour.
-          </li>
-          <li>
-            We provide peace of mind with our SRP (ship replacement program), so should you lose
-            your ship due to logi or FC error, you will be reimbursed.
-          </li>
-        </ul>
         <h3>What are Incursions?</h3>
         <p>
           Incursions are automated events introduced with the Incursion expansion in which the NPC
