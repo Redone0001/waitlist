64f532d93bc8ad4426b93d6f33d8e60f3d9b9394
Redone0001 <loick.coets@gmail.com>
2023-10-29 19:54:12 +0100

    [add] location box

--- backend/src/data/location.rs
+++ backend/src/data/location.rs
@@ -2,14 +2,15 @@ use crate::{
     app::Application,
     core::esi::{ESIError, ESIScope},
 };
-use eve_data_core::TypeLoc;
+use std::collections::HashMap
+use eve_data_core::TypeID;
 
-pub async fn get_location(app: &Application, character_id: i64) -> Result<TypeLoc, ESIError> {
+pub async fn get_location(app: &Application, character_id: i64) -> Result<HashMap, ESIError> {
     let path = format!("/v2/characters/{}/location/", character_id);
     let result = app
         .esi_client
         .get(&path, character_id, ESIScope::Current_Location)
         .await?;
 	println!("get_location result: {:?}", result);
-	Ok(result)
+	Ok()
 }
