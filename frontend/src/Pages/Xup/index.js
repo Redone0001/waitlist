2ce99542a3bf171d8d28cdf359396da6c984edb5
Redone0001 <loick.coets@gmail.com>
2023-10-29 21:00:51 +0100

    [add] location box

--- frontend/src/Pages/Xup/index.js
+++ frontend/src/Pages/Xup/index.js
@@ -83,6 +83,7 @@ export function Xup() {
   const [alt, setAlt] = React.useState(false);
   const [implants] = useApi(`/api/implants?character_id=${authContext.current.id}`);
   const loc = useApi(`/api/location?character_id=${authContext.current.id}`);
+  console.log(loc)
 
   const handleChange = () => {
     setAlt(!alt);
