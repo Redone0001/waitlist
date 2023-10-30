ec5e7c657ad6b1ff82f30b637f3971b26c74ee8a
Redone0001 <loick.coets@gmail.com>
2023-10-30 20:31:45 +0100

    [add] sys name

--- frontend/src/Pages/Waitlist/XCard.js
+++ frontend/src/Pages/Waitlist/XCard.js
@@ -178,10 +178,8 @@ function ShipDisplay({ fit, onAction }) {
   const loc = useApi(`/api/location?character_id=${fit.character.id}`)[0]
   if (loc && loc.solar_system_id) {
     console.log(loc.solar_system_id);
-    const systemName = getSystemName(loc.solar_system_id)
   } else {
     console.log("loc or loc.solar_system_id is null or undefined");
-    const systemName = "Unknow"
   }
   const namePrefix = fit.character ? `${fit.character.name}'s ` : "";
   if (fit.dna && fit.hull) {
@@ -222,7 +220,7 @@ function ShipDisplay({ fit, onAction }) {
                         Reject
                       </Button>
                       {loc && loc.solar_system_id ? (
-                        <span>Solar System: {systemName}</span>
+                        <span>Solar System: {getSystemName(loc.solar_system_id)}</span>
                       ) : null}
                     </Buttons>
                 </>
