d1ca7fb7339429b820622bb3994834b686e6ad4a
loick <loick.coets@gmail.com>
2024-01-08 13:31:39 +0000

    Revert "[add] location box"
    This reverts commit 5840a29a1e9a0c951d8340566046716c0b78c8dc.

--- backend/src/data/location.rs
+++ backend/src/data/location.rs
@@ -5,7 +5,7 @@ use crate::{
 use std::collections::HashMap;
 use eve_data_core::TypeID;
 
-pub async fn get_location(app: &Application, character_id: i64) -> Result<HashMap<String, i32>, ESIError> {
+pub async fn get_location(app: &Application, character_id: i64) -> Result<HashMap, ESIError> {
     let path = format!("/v2/characters/{}/location/", character_id);
     let result = app
         .esi_client
