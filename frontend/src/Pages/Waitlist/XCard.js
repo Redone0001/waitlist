8c35b3fdfbaebfef740336d21ca79d19e9d653b5
loick <loick.coets@gmail.com>
2024-01-08 13:31:28 +0000

    Revert "[add] solar name"
    This reverts commit cb01c0a611d59e865985bf0e813b159309716850.

--- frontend/src/Pages/Waitlist/XCard.js
+++ frontend/src/Pages/Waitlist/XCard.js
@@ -47,14 +47,14 @@ async function approveFit(id) {
 
 async function getSystemName(id) {
   const url_api = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility"
-  request_body = "[${id}]"
-  console.log(request_body)
+  body = "[${id}]"
+  console.log(body)
   return fetch(url_api, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: request_body,
+    body: ,
   })
     .then(response => {
       if (!response.ok) {
