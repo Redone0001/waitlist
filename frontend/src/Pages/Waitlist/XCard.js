0c3e7d9b931b8003d163aa18f68235ef90ffbed1
loick <loick.coets@gmail.com>
2024-01-08 13:31:23 +0000

    Revert "[add] sys name"
    This reverts commit d3eef7e97d26b8a2e4f3f98cd55ea64226526942.

--- frontend/src/Pages/Waitlist/XCard.js
+++ frontend/src/Pages/Waitlist/XCard.js
@@ -178,6 +178,8 @@ function ShipDisplay({ fit, onAction }) {
   const loc = useApi(`/api/location?character_id=${fit.character.id}`)[0]
   if (loc && loc.solar_system_id) {
     console.log(loc.solar_system_id);
+    const systemName = getSystemName(loc.solar_system_id).useState(null)
+    console.log(systemName)
   } else {
     console.log("loc or loc.solar_system_id is null or undefined");
   }
