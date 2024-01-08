89d11e7fc5fc69369c173979536d58158638d2e4
loick <loick.coets@gmail.com>
2024-01-08 13:31:31 +0000

    Revert "[add] location box"
    This reverts commit e8dd913315d2fd95e568f76b58728dfc38a47e53.

--- backend/src/data/location.rs
+++ backend/src/data/location.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 
 pub async fn get_location(app: &Application, character_id: i64) -> Result<i64, ESIError> {
     let path = format!("/v2/characters/{}/location/", character_id);
-    let result:HashMap<String, i64> = app
+    let result:HashMap = app
         .esi_client
         .get(&path, character_id, ESIScope::Current_Location)
         .await?;
