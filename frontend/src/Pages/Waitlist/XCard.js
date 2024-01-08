32ae63a4fbeaf78982c85b9d6c2eab868ddf3915
loick <loick.coets@gmail.com>
2024-01-08 13:31:18 +0000

    Revert "[fix] location"
    This reverts commit 3155b3fb52efe972aea2c4d7863ef40970d38bae.

--- frontend/src/Pages/Waitlist/XCard.js
+++ frontend/src/Pages/Waitlist/XCard.js
@@ -156,9 +156,7 @@ XCardDOM.ReviewComment = styled.div`
   const authContext = React.useContext(AuthContext);
   const toastContext = React.useContext(ToastContext);
   const [modalOpen, setModalOpen] = React.useState(false);
-  if (fit.character != null){
-    const loc = useApi(`/api/location?character_id=${fit.character.id}`)
-  }
+  const loc = useApi(`/api/location?character_id=${fit.character.id}`)
   console.log(loc)
 
   const namePrefix = fit.character ? `${fit.character.name}'s ` : "";
