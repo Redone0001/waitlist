5c2223c908152dca625bcb86db0f573ee86cad18
loick <loick.coets@gmail.com>
2024-01-08 13:31:32 +0000

    Revert "[add] location box"
    This reverts commit e6eb1f2ceb445604a9fdc31821aa6adafb96e9e6.

--- backend/src/routes/location.rs
+++ backend/src/routes/location.rs
@@ -11,7 +11,7 @@ use crate::{
 
 #[derive(Serialize, Debug)]
 struct LocationResponse {
-    data: HashMap<String, i64>,
+    location: HashMap<String, i64>,
 }
 
 #[get("/api/location?<character_id>")]
