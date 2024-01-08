25cbc08d9e790f23158d2759a26e68fb388a636a
loick <loick.coets@gmail.com>
2024-01-08 13:31:38 +0000

    Revert "[add] location box"
    This reverts commit e9d6b36bd1f6c3cf93e5840ac80eae7c0a63e009.

--- frontend/src/Pages/Waitlist/XCard.js
+++ frontend/src/Pages/Waitlist/XCard.js
@@ -195,7 +195,7 @@ function ShipDisplay({ fit, onAction }) {
                   </Button>
                 </Buttons>
                 <div>
-                  {loc}
+                {loc.solar_system_id}
                 </div>
               )}
 
