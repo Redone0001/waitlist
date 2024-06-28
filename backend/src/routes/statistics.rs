83a5da2943e4b91adb7f1f95f75aad3f3f0f7436
Redone0001 <loick.coets@gmail.com>
2024-06-28 15:33:01 +0200

    [fix] fc time last 28d

--- backend/src/routes/statistics.rs
+++ backend/src/routes/statistics.rs
@@ -374,7 +374,7 @@ impl Queries {
 			.into_iter()
 			.map(|row| (row.character_name, row.fleet_seconds as f64))
 			.collect())
-}
+	}
 }
 
 struct Displayer {}
@@ -484,36 +484,6 @@ impl Displayer {
         }
         result
     }
-	
-	fn build_fc_vs_time_28d(
-        source_fc: &BTreeMap<TypeID, f64>,
-        source_time: &BTreeMap<TypeID, f64>,
-    ) -> Result<BTreeMap<String, BTreeMap<&'static str, f64>>, Madness> {
-        let sum_fc: f64 = source_fc.values().sum();
-        let sum_time: f64 = source_time.values().sum();
-
-        let mut result = BTreeMap::new();
-
-        for (fc, &fc_count) in source_fc {
-            let mut fc_map = BTreeMap::new();
-            fc_map.insert("FC", fc_count / sum_fc);
-            fc_map.insert("Time", 0.);
-            result.insert(fc.clone(), fc_map);
-        }
-
-        for (fc, &time) in source_time {
-            if let Some(entry) = result.get_mut(fc) {
-                *entry.get_mut("Time").unwrap() += time / sum_time;
-            } else {
-                let mut fc_map = BTreeMap::new();
-                fc_map.insert("FC", 0.);
-                fc_map.insert("Time", time / sum_time);
-                result.insert(fc.clone(), fc_map);
-            }
-        }
-
-        Ok(result)
-    }
 }
 
 #[derive(Serialize)]
@@ -527,7 +497,7 @@ struct StatsResponse {
     x_vs_time_by_hull_28d: BTreeMap<String, BTreeMap<&'static str, f64>>,
     time_spent_in_fleet_by_month: BTreeMap<YearMonth, BTreeMap<&'static str, f64>>,
 	fleet_seconds_by_fc_by_month: BTreeMap<YearMonth, BTreeMap<String, f64>>,
-	fleet_seconds_by_fc_28d: BTreeMap<String, BTreeMap<&'static str, f64>>,
+	fleet_seconds_by_fc_28d: BTreeMap<String, f64>,
 
 }
 
@@ -566,10 +536,7 @@ async fn statistics(
             &seconds_by_character_month,
         ),
 		fleet_seconds_by_fc_by_month: Displayer::build_fleet_seconds_by_fc_by_month(&seconds_by_fc_month),
-		fleet_seconds_by_fc_28d: Displayer::build_fc_vs_time_28d(
-            &seconds_by_fc_28d,
-            &seconds_by_fc_28d, // Assuming you have separate maps for FC and time data, otherwise use the same map
-        )?,
+		fleet_seconds_by_fc_28d: seconds_by_fc_28d,
     }))
 }
 
