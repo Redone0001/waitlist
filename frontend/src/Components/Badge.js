b77537375a23b46e3c52ea1cee10a2dfa32acb27
Redone0001 <loick.coets@gmail.com>
2024-07-16 20:39:57 +0200

    [fix] mango image

--- frontend/src/Components/Badge.js
+++ frontend/src/Components/Badge.js
@@ -102,7 +102,7 @@ export const icons = {
   },
   MULTIBOX: {type: "shield", color: "dark", letter: "M", name: "Multibox"},
   DEAF_BEAN: {type: "shield", color: "red", letter: "D", name: "DEAF_BEAN"},
-  MANGO: { type: "image", href: require("../Pages/Guide/badges/c.png"), name: "Mango" },
+  MANGO: { type: "image", href: require("../Pages/Guide/badges/mango.png"), name: "Mango" },
   STARTER: { type: "shield", color: "neutral", letter: "S", name: "Starter pilot" },
   UNKNOWN: { type: "shield", color: "neutral", letter: "?", name: null },
 };
