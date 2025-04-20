d54d29f9cbd035c6722020d85d81dff45dbbc99b
Loick coets <loick.coets@gmail.com>
2025-04-20 21:48:52 +0200

    [add] filter on stat

--- frontend/src/Pages/FC/Statistics.js
+++ frontend/src/Pages/FC/Statistics.js
@@ -553,19 +553,18 @@ export function Statistics() {
   const monthOptions = [3, 6, 12, 24, 36];
 
   return (
-    <>
-      <div style={{ marginBottom: "1rem" }}>
-        <label>
-          Show last&nbsp;
-          <select value={monthLimit} onChange={handleMonthLimitChange}>
-            {monthOptions.map((opt) => (
-              <option key={opt} value={opt}>
-                {opt} months
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
+	<div style={{ marginBottom: "1rem" }}>
+	<label>
+	  Show last&nbsp;
+	  <select value={monthLimit} onChange={handleMonthLimitChange}>
+		{monthOptions.map((opt) => (
+		  <option key={opt} value={opt}>
+			{opt} months
+		  </option>
+		))}
+	  </select>
+	</label>
+	</div>
     <Row>
       <Graph>
         <FleetTimeByMonth data={statsData.fleet_seconds_by_month} />
