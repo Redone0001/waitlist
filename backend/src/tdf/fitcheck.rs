358ae8a67cf9360ead52783af598b0b315da32aa
Loick coets <loick.coets@gmail.com>
2025-04-21 01:22:47 +0200

    [fix] rust

--- backend/src/tdf/fitcheck.rs
+++ backend/src/tdf/fitcheck.rs
@@ -145,21 +145,21 @@ impl<'a> FitChecker<'a> {
         {
             self.tags.insert("NO-EM-805");
 			self.approved = false;
-			self.errors.push("Missing required implants");
+			self.errors.push("Missing required implants".to_string());
         }
 		if (self.fit.hull == type_id!("Oneiros"))
             && (!self.pilot.implants.contains(&type_id!("% RA-704")) || !self.pilot.implants.contains(&type_id!("% FC-804")) || !self.pilot.implants.contains(&type_id!("% EO-604")))
         {
             self.tags.insert("Missing Oneiros implants");
 			self.approved = false;
-			self.errors.push("Missing required implants");
+			self.errors.push("Missing required implants".to_string());
         }
 		if (self.fit.hull == type_id!("Leshak"))
             && (!self.pilot.implants.contains(&type_id!("% RF-906")))
         {
             self.tags.insert("NO-RF-906");
 			self.approved = false;
-			self.errors.push("Missing required implants");
+			self.errors.push("Missing required implants".to_string());
         }
     }
 
